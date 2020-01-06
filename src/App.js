028f6b63c4ed0f220755d29297f2d491ff7741f6
Wesley Berry <wesleyberry52@gmail.com>
2020-01-06 11:37:30 -0600

    set up store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 import Posts from './components/Posts';
 import PostsForm from './components/Postsform';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostsForm />
-      <h2 />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostsForm />
+        <h2 />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
